bfa5bf51344bd3b85bff0416d0431fbe5aa8fece
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-09-23 19:37:21 +0530

    Dates Component Completed

--- Client/src/components/RouteingTable/Authors/Dates.jsx
+++ Client/src/components/RouteingTable/Authors/Dates.jsx
@@ -1,51 +1,64 @@
 import React, { Components, useState } from 'react';
 import PageBanner from '../PageBanner';
 import Date from '../../../JSON/date.json';
+import Table from 'react-bootstrap/Table'
 
 
 
-function Dates ()
-{
+function Dates() {
     return (
         <div>
             <PageBanner name="Important Dates" head="Author" subhead="Important Dates" info="Feel Free To Get In Touch" />
             <div id="content">
-      <div className="container">
-        <div className="page-content">
+                <div className="container">
+                    <div className="page-content">
 
-         <div className="col-md-9">
+                        <div className="col-md-9">
+                            <div>   
+                                <br/>
+                            <h1 className="accent-color">WCE Research Symposium on Computing - RSC 2021</h1><br/>
+                            <p><strong>Organized by WCE ACM Student Chapter <br />National  Level Event By Department of Computer Science & Engineering</strong></p>
+                            </div> 
+                            <h1 className="accent-color">Important Dates</h1>
+                            <br/>
+                            <Table className="table table-responsive table-condensed table-bordered"><br />
+                                <thead>
+                                    <th>Sr No</th>
+                                    <th>Important Dates</th>
+                                    <th>Particulars</th>
+                                </thead>
+                                <tbody>
+                                    {
+                                        Date.map(Dates => <tr key={Dates.id}>
 
-          <h1 className="accent-color">WCE Research Symposium on Computing - RSC 2021</h1>
-          <p><strong>Organized by WCE ACM Student Chapter <br/>National  Level Event By Department of Computer Science & Engineering</strong></p>
+                                            <td>{Dates.id} </td>
 
-          <h1 className="accent-color">Important Dates</h1>
-		
-          <table className="table table-responsive table-condensed table-bordered"><br/>
-           <thead>
-            <th>Sr No</th>
-            <th>Important Dates</th>
-            <th>Particulars</th>
-          </thead>
-          
-              {
-          Date.map (Dates => <tr key={Dates.id}>
-              
-              <td>{Dates.id} </td>
-              
-               <td>{Dates.impDate}</td> 
-               <td>{Dates.details}</td>
-               
-               
-               </tr>)
-              }
-        </table>
-      </div>
-      </div>
-      </div>
-      </div>
-        </div>
+                                            <td>{Dates.impDate}</td>
+                                            <td>{Dates.details}</td>
 
-    );
+
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </Table>
+                        </div>
+                        <div class="col-md-3 sidebar right-sidebar">
+
+                            <div class="widget widget-categories">
+                                <h4>Related Links <span class="head-line"></span></h4>
+                                <ul>
+                                    
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+
+            );
 }
 
-export default Dates;
+            export default Dates;
