c3ad12198d5328d2931690a59a7042c835539088
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-09-30 18:20:49 +0530

    Add . Organisation Committee Component

--- Client/src/components/RouteingTable/Organisation/Org_committee.jsx
+++ Client/src/components/RouteingTable/Organisation/Org_committee.jsx
@@ -1,23 +1,53 @@
 import React, { Components, useState } from 'react';
-
+import Table from 'react-bootstrap/Table'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
-  } from "react-router-dom";
-  import PageBanner from '../PageBanner';
+} from "react-router-dom";
+import PageBanner from '../PageBanner';
+import org from '../../../JSON/org_com.json';
 
 
 
 
 
-function Org_Committee()
-{
+function Org_Committee() {
     return (
         <div>
             <PageBanner name="Organising Committee" head="Organisation" subhead="Organising Committee" info="We Are WCE Research Symposium" />
-            
+            <div id="content">
+                <div className="container">
+                    <div className="page-content">
+                        <br />
+                        <br />
+                        <div className="col-md-9">
+                            <Table className="table table-responsive table-condensed table-bordered">
+                                <thead>
+                                    <th>Role</th>
+                                    <th>Name</th>
+                                    <th>Designation</th>
+                                </thead>
+                                <tbody>
+                                    {
+                                        org.map(Entry => <tr key={org.id}>
+
+                                            <td>{Entry.role}</td>
+
+                                            <td>{Entry.name}</td>
+                                            <td>{Entry.designation}</td>
+
+
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </Table>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
