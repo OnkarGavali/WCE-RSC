b6e6a80c747784f38c2c2257b5b18f37ac466f5c
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-10-29 15:34:01 +0530

    Add. Login Page

--- Client/src/components/RouteingTable/Admin/Login.jsx
+++ Client/src/components/RouteingTable/Admin/Login.jsx
@@ -11,8 +11,44 @@ import PageBanner from '../PageBanner';
 function Login() {
     return (
         <div>
-            <PageBanner name="Login" head="" subhead=""  />
-            
+            <PageBanner name="Login" />
+            <div id="content">
+                <div class="container">
+                    <div class="page-content">
+                        <div class="col-md-5">
+
+                            <br/>
+                            <h4 class="classic-title"><span>Login for Admin Panel</span></h4>
+
+                            <form role="form" class="contact-form" id="contact-form2" method="post"
+                                action="<?php echo $acm->GetSelfScript(); ?>">
+                                <form id='login' method='post' accept-charset='UTF-8'>
+                                    <input type='hidden' name='submitted' id='submitted' value='1' />
+
+                                    <div class="form-group">
+                                        <div class="controls">
+                                            <input type="email" placeholder="Registered Email ID" name="email" id="email" class="email"
+                                                required="required" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="controls">
+                                            <input type="password" class="email" placeholder="Password" name="password" id="password"
+                                                required="required" />
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" id="submit" class="btn-system btn-large">Login</button>
+                                    <br/>
+                                    <br/>
+                                </form>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 
