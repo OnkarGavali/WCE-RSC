baa4cd8ad533adf9e50407ef2cd9a89ee9b3bef3
“OnkarGavali” <onkargavali1561@gmail.com>
2021-11-01 18:25:32 +0530

    orgnasation code cleaned

--- Client/src/components/RouteingTable/Organisation/Advisory_committee.jsx
+++ Client/src/components/RouteingTable/Organisation/Advisory_committee.jsx
@@ -14,36 +14,33 @@ import adv from '../../../JSON/adv_com.json';
 function Adv_Committee() {
     return (
         <div>
+            {/* PageBanner - start */}
             <PageBanner name="Advisory Committee" head="Organisation" subhead="Advisory Committee" info="We Are WCE Research Symposium" />
+            {/* PageBanner - end */}
+            
 
             <div className="contenti">
                 <div className="container">
                     <div className="page-content">
                        
+                        {/* Main Content - start */}
                         <div className="col-md-9">
                             <h1 className="accent-color">Advisory Committee</h1>
                             <br />
                             <table className="table table-responsive table-condensed table-bordered">
                                 <thead>
-                                    
                                     <th>Name</th>
-                                   
                                 </thead>
                                 <tbody>
                                     {
                                         adv.map(Entry => <tr key={adv.id}>
-
-                                            
-
                                             <td>{Entry.name}</td>
-                                            
-
-
                                         </tr>)
                                     }
                                 </tbody>
                             </table>
                         </div>
+                        {/* Main Content - end */}
                         
                         {/* Related Links - start */}
                         <div className ="col-md-3 sidebar right-sidebar">
@@ -59,11 +56,10 @@ function Adv_Committee() {
                         </div>
                         {/* Related Links - end */}
 
+
                     </div>
                 </div>
             </div>
-
-
         </div>
     );
 }
