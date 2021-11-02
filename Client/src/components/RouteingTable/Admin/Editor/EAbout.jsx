f3a1ebcb65af9a8af6c3539d0391a0b40551e7ef
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-11-02 17:07:18 +0530

    Admin Panel : About Page Updations

--- Client/src/components/RouteingTable/Admin/Editor/EAbout.jsx
+++ Client/src/components/RouteingTable/Admin/Editor/EAbout.jsx
@@ -2,7 +2,7 @@
 import React, {useState } from 'react'
 
 import PageBanner from '../../PageBanner';
-import data from '../../../../JSON/about.json'
+import abt from '../../../../JSON/About/about.json'
 
 
 
@@ -21,12 +21,10 @@ function handlePasswordChange() {
 
 function EAbout() {
     const [state, setState] = useState({
-        head: data.aboutHead[0].title,
-        aboutRSCP1: data.aboutRSC[0].para1,
-        aboutRSCP2: data.aboutRSC[0].para2,
-        aboutRSCP3: data.aboutRSC[0].para3,
-        aboutWCE:data.aboutWCE[0].para1,
-        aboutDept:data.aboutDept[0].para1
+       
+        RSC: abt.data.aboutRSC,
+        WCE: abt.data.aboutWCE,
+        Dept: abt.data.aboutDept
 
     });
 
@@ -41,13 +39,16 @@ function EAbout() {
 
     const handleSubmit = evt=>{
        evt.preventDefault();
-        let dataToSend = {
-            title: state.title
+
+        let abtToSend = {
+           RSC : state.RSC,
+           WCE : state.WCE,
+           Dept : state.Dept
          
 
 
         };
-        console.log(JSON.stringify(dataToSend));
+        console.log((abtToSend));
 
 
 
@@ -58,10 +59,10 @@ function EAbout() {
         <div>
 
             <PageBanner name="About" head='Admin Panel' />
-            <div id="content">
+            <div className="contenti">
                 <div className="container">
                     <div className="page-content">
-                        <div className="col-md-6">
+                        <div className="col-md-8">
 
                             <br />
                             <h2 className="classic-title"><span>Edit About Page </span></h2>
@@ -70,46 +71,28 @@ function EAbout() {
                             <form id='login' accept-charset='UTF-8' onSubmit={handleSubmit}>
                                 <input type='hidden' name='submitted' id='submitted' value='1' />
 
+                                
                                 <div className="form-group">
                                     <div className="controls">
-                                        <h3 style={{marginBottom:'9px'}}>Title : </h3>
-                                        <textarea type="text" value={state.head}   name="head" className="email"
+                                        <h3 className="classic-title"><span> AboutRSC : </span></h3>
+                                        <textarea type="text" value={state.RSC} rows="30"  name="aboutRSC" className="email"
                                             required="required"  onChange={handleChange} style={{}}/>
                                     </div>
                                 </div>
-                                <div className="form-group">
-                                    <div className="controls">
-                                        <h3 style={{marginBottom:'9px'}}>AboutRSC Para 1  : </h3>
-                                        <textarea type="text" value={state.aboutRSCP1} rows="6"  name="aboutRSCP1" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="controls">
-                                        <h3 style={{marginBottom:'9px'}}>AboutRSC Para 2 : </h3>
-                                        <textarea type="text" value={state.aboutRSCP2} rows="6" name="aboutRSCP2" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="controls">
-                                        <h3 style={{marginBottom:'9px'}}>AboutRSC Para 3 : </h3>
-                                        <textarea type="text" value={state.aboutRSCP3} rows="6"   name="aboutRSCP3" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
-                                    </div>
-                                </div>
+                                
+                                
                               
                                 <div className="form-group">
                                     <div className="controls">
-                                        <h3 style={{marginBottom:'9px'}}>About WCE : </h3>
-                                        <textarea type="text" value={state.aboutWCE} rows="6" name="aboutWCE" className="email"
+                                        <h3 className="classic-title" > <span> About WCE : </span></h3>
+                                        <textarea type="text" value={state.WCE} rows="15" name="aboutWCE" className="email"
                                             required="required"  onChange={handleChange} style={{}}/>
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <div className="controls">
-                                        <h3 style={{marginBottom:'9px'}}>About Dept : </h3>
-                                        <textarea type="text" value={state.aboutDept} rows="6" name="aboutDept" className="email"
+                                        <h3 className="classic-title" > <span>About Dept : </span></h3>
+                                        <textarea type="text" value={state.Dept} rows="15" name="aboutDept" className="email"
                                             required="required"  onChange={handleChange} style={{}}/>
                                     </div>
                                 </div>
