d3490f7db2056a6150dba5f7d027f4c2364571f1
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-11-20 19:15:15 +0530

    Admin Panel : Poster Presentation Backend Connection

--- Client/src/components/RouteingTable/Admin/Editor/Programs/EPosterPre.jsx
+++ Client/src/components/RouteingTable/Admin/Editor/Programs/EPosterPre.jsx
@@ -1,42 +1,105 @@
 
-import React, {useState } from 'react'
-import info from '../../../../../JSON/Programs/posterPresentation.json'
-import { NoticeBoard } from '../../NoticeBoard';
+import React, { useState, Fragment, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+
+import { NoticeBoard } from "../../NoticeBoard";
+import axios from "axios";
 
 function EPosterPre() {
-    const [state, setState] = useState({
-       sizeLayout: info.data.sizeAndLayout,
-       orgIdea: info.data.organisationOfIdeas,
-       format:info.data.format,
-       presentation:info.data.presentation 
-        
-    });
-    const [displayNotice, setdisplayNotice] = useState(false);
+
+    const [sizeLayout, setSizeLayout] = useState();
+    const [orgIdea, setOrgIdea] = useState();
+    const [format, setFormat] = useState();
+    const [presentation, setPresentation] = useState();
+    const [allData, setAllData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [displayNotice, setDisplayNotice] = useState(false);
     const [displayeNoticeHead, setDisplayeNoticeHead] = useState('');
     const [displayeNoticeContent, setDisplayeNoticeContent] = useState('')
-    const [maintainanceBreak, setMaintainanceBreak] = useState(false);
-    const [maintainanceBreakHead, setMaintainanceBreakHead] = useState('');
-    const [maintainanceBreakContent, setMaintainanceBreakContent] = useState('');
+    const [maintenanceBreak, setMaintenanceBreak] = useState(false);
+    const [maintenanceBreakHead, setMaintenanceBreakHead] = useState('');
+    const [maintenanceBreakContent, setMaintenanceBreakContent] = useState('');
+    const [finalData, setFinalData] = useState(null);
+    const [finalMessage, setFinalMessage] = useState("");
+
+    useEffect(() => {
+        const getData = async () => {
+            await axios.get(
+                "http://localhost:5000/get/posterPresentation"
+            ).then((response)=>{
+                if(response.data[0]){
+                    setAllData(response.data[0]);
+                }
+               setIsLoading(false);
+            }).catch((e)=>{
+             /* HANDLE THE ERROR (e) */
+                console.log(e);
+                setIsLoading(false);
+            });
+        };
+        getData();
+        setIsLoading(false);
+    },[])
 
 
-    const handleChange = evt => {
-        const name = evt.target.name;
-        const value = evt.target.value;
-        setState({
-            ...state,
-            [name]: value
-        })
+    useEffect(() => {
+        if(!isLoading){
+            setDisplayNotice(allData.displayNoticeStatus)
+            setDisplayeNoticeHead(allData.displayeNoticeHeading)
+            setDisplayeNoticeContent(allData.displayeNoticeContent)
+            setMaintenanceBreak(allData.maintenanceBreakStatus)
+            setMaintenanceBreakHead(allData.maintenanceBreakHeading)
+            setMaintenanceBreakContent(allData.maintenanceBreakContent)
+            if(allData.data){
+              setSizeLayout(allData.data.sizeAndLayout)
+              setOrgIdea(allData.data.organisationOfIdeas)
+              setFormat(allData.data.format)
+              setPresentation(allData.data.presentation)
+
+
+            }      
+        }
+    }, [allData])
+
+    const endFormater = () => {
+        const final = {
+            "displayNoticeStatus":displayNotice,
+            "displayNoticeHeading":displayeNoticeHead,
+            "displayNoticeContent":displayeNoticeContent,
+            "maintenanceBreakStatus":maintenanceBreak,
+            "maintenanceBreakHeading":maintenanceBreakHead,
+            "maintenanceBreakContent":maintenanceBreakContent,
+            "data":{
+               "sizeAndLayout": sizeLayout,
+               "organisationOfIdeas":orgIdea,
+               "format":format,
+               "presentation":presentation
+
+            }
+        }
+        setFinalData(final)
     }
+
+    
+    const uploadContent = () => {
+        const headers = { 
+            'x-access-token': localStorage.getItem("x-access-token")
+        };
+        axios.put('http://localhost:5000/put/posterPresentation/618e38ec67d01ef1b504194f', finalData, { headers })
+            .then(response => console.log(response));
+    }
+
+    useEffect(() => {
+        if(finalData){
+            uploadContent()
+        }
+    }, [finalData])
 
     const handleSubmit = evt=>{
        evt.preventDefault();
-        let dataToSend = {
-           sizeLayout: state.sizeLayout,
-            
-
-
-        };
-        console.log(JSON.stringify(dataToSend));
+       endFormater();
 
 
 
@@ -56,37 +119,37 @@ function EPosterPre() {
                                 <div className="form-group">
                                     <div className="controls">
                                     <h2 className="classic-title"><span>Size & Layout </span></h2>
-                                    <textarea type="text" value={state.sizeLayout} rows="10" placeholder={state.sizeLayout} name="sizeLayout" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
+                                    <textarea type="text" value={sizeLayout} rows="10"  name="sizeLayout" className="email"
+                                            required="required"  onChange={(e)=>setSizeLayout(e.target.value)} style={{}}/>
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <div className="controls">
                                     <h2 className="classic-title"><span>Organization of Ideas </span></h2>
 
-                                        <textarea type="text" value={state.orgIdea} rows="10"   name="orgIdea" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
+                                        <textarea type="text" value={orgIdea} rows="10"   name="orgIdea" className="email"
+                                            required="required"  onChange={(e)=>setOrgIdea(e.target.value)} style={{}}/>
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <div className="controls">
                                     <h2 className="classic-title"><span>Format </span></h2>
 
-                                        <textarea type="text" value={state.format} rows="10" name="format" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
+                                        <textarea type="text" value={format} rows="10" name="format" className="email"
+                                            required="required"  onChange={(e)=>setFormat(e.target.value)} style={{}}/>
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <div className="controls">
                                     <h2 className="classic-title"><span>Presentation </span></h2>
 
-                                        <textarea type="text" value={state.presentation} rows="10"   name="presentation" className="email"
-                                            required="required"  onChange={handleChange} style={{}}/>
+                                        <textarea type="text" value={presentation} rows="10"   name="presentation" className="email"
+                                            required="required"  onChange={(e)=>setPresentation(e.target.value)} style={{}}/>
                                     </div>
                                 </div>
                               
-                                <NoticeBoard title={'Display Notice'} titleMessage={'Notice is : '} noticeState ={displayNotice} noticeStateChange={setdisplayNotice} noticeHead={displayeNoticeHead} noticeHeadChange={setDisplayeNoticeHead} noticeContent={displayeNoticeContent} noticeContentChange={setDisplayeNoticeContent} headLabel={'Notice Heading'} contentLabel={'Notice Content'} />
-                                <NoticeBoard title={'Maintainance Break'} titleMessage={'Maintainance break is : '} noticeState ={maintainanceBreak} noticeStateChange={setMaintainanceBreak} noticeHead={maintainanceBreakHead} noticeHeadChange={setMaintainanceBreakHead} noticeContent={maintainanceBreakContent} noticeContentChange={setMaintainanceBreakContent} headLabel={'Maintainance Break Heading'} contentLabel={'Maintainance Break Message Content'} />           
+                                <NoticeBoard title={'Display Notice'} titleMessage={'Notice is : '} noticeState ={displayNotice} noticeStateChange={setDisplayNotice} noticeHead={displayeNoticeHead} noticeHeadChange={setDisplayeNoticeHead} noticeContent={displayeNoticeContent} noticeContentChange={setDisplayeNoticeContent} headLabel={'Notice Heading'} contentLabel={'Notice Content'} />
+                                <NoticeBoard title={'Maintainance Break'} titleMessage={'Maintainance break is : '} noticeState ={maintenanceBreak} noticeStateChange={setMaintenanceBreak} noticeHead={maintenanceBreakHead} noticeHeadChange={setMaintenanceBreakHead} noticeContent={maintenanceBreakContent} noticeContentChange={setMaintenanceBreakContent} headLabel={'Maintainance Break Heading'} contentLabel={'Maintainance Break Message Content'} />           
 
                                 <div  style={{ textAlign: 'right' }}>
                                 <button type="submit" id="submit" className="btn-system btn-large">Update Content</button>
--- Client/src/components/RouteingTable/Programs/PosterPresentation.jsx
+++ Client/src/components/RouteingTable/Programs/PosterPresentation.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PageBanner from '../PageBanner';
+import axios from 'axios';
 import {
     Link
 } from "react-router-dom";
@@ -13,24 +14,78 @@ function PosterPresentation() {
     const [displayNoticeStatus, setDisplayNoticeStatus] = useState(false);
     const [toShow, setToShow] = useState(true);
     const [isLoading, setIsLoading] = useState(true);
+    const [sizeLayout, setSizeLayout] = useState();
+    const [orgIdea, setOrgIdea] = useState();
+    const [format, setFormat] = useState();
+    const [presentation, setPresentation] = useState();
+    const [displayNotice, setDisplayNotice] = useState(false);
+    const [displayeNoticeHead, setDisplayeNoticeHead] = useState('');
+    const [displayeNoticeContent, setDisplayeNoticeContent] = useState('')
+    const [maintenanceBreak, setMaintenanceBreak] = useState(false);
+    const [maintenanceBreakHead, setMaintenanceBreakHead] = useState('');
+    const [maintenanceBreakContent, setMaintenanceBreakContent] = useState('');
+    const [finalData, setFinalData] = useState(null);
+    const [finalMessage, setFinalMessage] = useState("");
+
 
     useEffect(() => {
-        setAllData(posterPresentation);
-        if(allData.maintenanceBreakState){
-            setToShow(false);
-        }else{
-            setToShow(true);
+        const getData = async () => {
+            await axios.get(
+                "http://localhost:5000/get/posterPresentation"
+            ).then((response) => {
+                if (response.data[0]) {
+                    setAllData(response.data[0]);
+                } else {
+                    setMaintenanceBreak(true);
+                    setMaintenanceBreakHead("Problem in Fetching data")
+                    setMaintenanceBreakContent("Please Contact admin for details")
+                }
+            }).catch((e) => {
+                /* HANDLE THE ERROR (e) */
+                console.log(e);
+                setMaintenanceBreak(true);
+                setMaintenanceBreakHead("Problem in Fetching data")
+                setMaintenanceBreakContent("Please Contact admin for details")
+            });
+            setIsLoading(false);
+        };
+        getData();
+        setIsLoading(false);
+    }, [])
+
+
+    useEffect(() => {
+        if (!isLoading) {
+            if (allData.maintenanceBreakStatus) {
+                setToShow(false);
+                setMaintenanceBreak(allData.maintenanceBreakStatus)
+                setMaintenanceBreakHead(allData.maintenanceBreakHeading)
+                setMaintenanceBreakContent(allData.maintenanceBreakContent)
+            } else {
+                setToShow(true);
+                if (allData.displayNoticeStatus) {
+                    setDisplayNotice(allData.displayNoticeStatus)
+                    setDisplayeNoticeHead(allData.displayNoticeHeading)
+                    setDisplayeNoticeContent(allData.displayNoticeContent)
+                }
+                if (allData.data) {
+                    setSizeLayout(allData.data.sizeAndLayout)
+                    setOrgIdea(allData.data.organisationOfIdeas)
+                    setFormat(allData.data.format)
+                    setPresentation(allData.data.presentation)
+                }
+            }
         }
-        setIsLoading(false)
-        
+
     }, [allData])
+
 
     return (
         <div>
             {/* PageBanner - start */}
             <PageBanner name="Poster Presentations" head="Programs" subhead="Poster Presentations" info="Feel Free To Get In Touch" />
             {/* PageBanner - end */}
-            
+
             <div className="contenti">
                 <div className="container">
                     <div className="page-content">
@@ -39,29 +94,43 @@ function PosterPresentation() {
                         <div className="col-md-9" style={{ textAlign: "justify" }}>
                             {
                                 isLoading ? (
-                                        <div>Loading</div>
+                                    <div>Loading</div>
                                 ) : (
                                     <>
-                                        <h3 className="classic-title"><span>Size & Layout</span></h3>
                                         {
-                                            allData.data.sizeAndLayout && <p style={{whiteSpace:'pre-line'}}>{allData.data.sizeAndLayout}</p>
-                                        }
-                                        <img src="/images/poster.png" alt="Poster" style={{margin:"10px"}}/>
-                                        
-                                        
-                                        <h3 className="classic-title"><span>Organization of Ideas</span></h3>
-                                        {
-                                            allData.data.organisationOfIdeas && <p style={{whiteSpace:'pre-line'}}>{allData.data.organisationOfIdeas}</p>
-                                        }
+                                            toShow ? (
+                                                <React.Fragment>
+                                                    {
+                                                        displayNotice ? (
+                                                            <displayNotice heading={displayeNoticeHead} message={displayeNoticeContent} />
+                                                        ) : null
+                                                    }
+                                                    <h3 className="classic-title"><span>Size & Layout</span></h3>
+                                                    {
+                                                        sizeLayout && <p style={{ whiteSpace: 'pre-line' }}>{sizeLayout}</p>
+                                                    }
+                                                    <img src="/images/poster.png" alt="Poster" style={{ margin: "10px" }} />
 
-                                        <h3 className="classic-title"><span>Format</span></h3>
-                                        {
-                                            allData.data.format && <p style={{whiteSpace:'pre-line'}}>{allData.data.format}</p>
+
+                                                    <h3 className="classic-title"><span>Organization of Ideas</span></h3>
+                                                    {
+                                                        orgIdea && <p style={{ whiteSpace: 'pre-line' }}>{orgIdea}</p>
+                                                    }
+
+                                                    <h3 className="classic-title"><span>Format</span></h3>
+                                                    {
+                                                        format && <p style={{ whiteSpace: 'pre-line' }}>{format}</p>
+                                                    }
+                                                    <h3 className="classic-title"><span>Presentation</span></h3>
+                                                    {
+                                                       presentation && <p style={{ whiteSpace: 'pre-line' }}>{presentation}</p>
+                                                    }
+                                                </React.Fragment>
+                                            ) : (
+                                                <maintenanceBreak heaading={maintenanceBreakHead} message={maintenanceBreakContent} />
+                                            )
                                         }
-                                        <h3 className="classic-title"><span>Presentation</span></h3>
-                                        {
-                                            allData.data.presentation && <p style={{whiteSpace:'pre-line'}}>{allData.data.presentation}</p>
-                                        }                               
+                            
                                     </>
                                 )
                             }
