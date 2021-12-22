994cf44ff4727a852406eaead44e435e97bacb1e
“OnkarGavali” <onkargavali1561@gmail.com>
2021-12-22 20:33:03 +0530

    added developer names in footer

--- React Frontend/src/components/Footer/CopyRight.jsx
+++ React Frontend/src/components/Footer/CopyRight.jsx
@@ -5,15 +5,16 @@ export const CopyRight = () => {
         <div className="copyright-section">
           <div className="row">
             <div className="col-md-6">
-              <p>&copy; 2022 WCE Research Symposium on Computing - All Rights Reserved <br/></p>
+              <p>&copy;  Department of Computer Science and Engineering, WCE, Sangli  - All Rights Reserved <br/></p>
               </div>
               <div className="col-md-6">
+                
                 <ul className="footer-nav">
-                  {/*
-                  <li><a href="sitemap.html">Sitemap</a></li>
-                  <li><a href="privacy.html">Privacy Policy</a></li>
-                  <li><a href="contact.html">Contact</a></li>
-                */}
+                  <li style={{cursor:"default"}}>Developed by : </li>
+                  <li><a href="https://www.linkedin.com/in/onkar-gavali/" target="_blank">Onkar Gavali</a></li>
+                  <li><a href="https://www.linkedin.com/in/aditya-sarnobat-16123b18b/" target="_blank">Aditya Sarnobat</a></li>
+                  <li><a href="https://www.linkedin.com/in/shreyash-malu-ab0ab51b1/" target="_blank">Shreyash Malu</a></li>
+               
                 </ul>
               </div>
             </div>
