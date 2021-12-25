3dddd4341d25292af4c6311d47c9b9ebe68acd5e
“OnkarGavali” <onkargavali1561@gmail.com>
2021-12-25 13:52:26 +0530

    header updated-image added

--- React Frontend/src/components/Header.jsx
+++ React Frontend/src/components/Header.jsx
@@ -52,12 +52,16 @@ function Header() {
    
         <div className="container">
           <div style={{ marginBottom: '-4%',display:'flex',justifyContent:'space-between' }} className='headercontainer' >
-              <div  className='headerLogo1'><img src="/images/WCE75.jpeg" alt="IFERP" style={{ maxWidth: '100px', marginTop: '5%', width:'20%',minWidth:"70px" }} /></div>
-              <div>
-                <h1 style={{paddingTop:'1%' , textAlign:'center' , fontSize:'30px',fontWeight:"700"}} className='headertitle'> {data.title}</h1> 
-                <h5  style={{  paddingTop:'1%', fontSize:'15px' , textAlign:'center' , color:'gray', whiteSpace:'pre-line',fontWeight:"700"}} className='headersubtitle'> {data.subtitle} </h5>
-                </div>
-              <div  className='headerLogo'><img src="https://static.wixstatic.com/media/e192f7_f60a1d7e58ba4708b9d94820fe99b0e2~mv2.png/v1/fill/w_300,h_300,al_c,q_85,usm_0.66_1.00_0.01/Drawing2.webp" alt="IFERP" style={{maxWidth: '100px', marginTop: '5%', width:'20%',minWidth:"70px" }} /></div>
+              <div  className='headerLogo1' >
+                <img src="/images/WCE75.jpeg" alt="IFERP" style={{  width:'100%',minWidth:"70px" }} />
+              </div>
+              <div style={{margin:"0", width: "100%"}}>
+                <h1 style={{margin:"10px", paddingTop:'1%' , textAlign:'center' , fontSize:'50px',fontWeight:"700", whiteSpace:'pre-line',letterSpacing: "3px"}} className='headertitle'> {data.title}</h1> 
+                <h2  style={{  paddingTop:'2%', textAlign:'center' , color:'gray', whiteSpace:'pre-line',fontWeight:"700"}} className='headersubtitle'> {data.subtitle} </h2>
+              </div>
+              <div  className='headerLogo' >
+                <img src="https://static.wixstatic.com/media/e192f7_f60a1d7e58ba4708b9d94820fe99b0e2~mv2.png/v1/fill/w_300,h_300,al_c,q_85,usm_0.66_1.00_0.01/Drawing2.webp" alt="IFERP" style={{  width:'100%',minWidth:"70px" }} />
+              </div>
               
              
           </div>
